10a5722f0af3b59f26c9b8232d8ffedb904ff4f8
Eleanor H <eleanorhasenbeck@duck.com>
2023-02-22 16:35:33 -0700

    Cleaning SWSI
    2010 SWSI data now in same format as 1980s SWSI data

--- scripts/CombininedSWSIdata.R
+++ scripts/CombininedSWSIdata.R
@@ -6,32 +6,21 @@
 library(tidyverse)
 library(dplyr)
 library(lubridate)
-install.packages("zoo")
 library(zoo)
+
 SWSI1981to2011 <- read.csv("data/raw/SWSI1981to2011.csv")
 SWSI2010tonow <- read.csv("data/raw/SWSI2010tonow.csv")
 
 
 ####Cleaning SWSI 2010 to now data####
-
-SWSI2010tonowCLEAN <- SWSI2010tonow %>%
-  select("basin","report_year","report_month","swsi") %>%
-  pivot_wider(names_from = "basin", values_from = "swsi")
   
 SWSI2010tonowCLEAN <- SWSI2010tonow %>%
-  select("basin","report_year","report_month","swsi") %>%
-  pivot_wider(names_from = "basin", values_from = "swsi") %>%
-  unite(Date, report_year,report_month) 
-  ?lubridate
-  
-  mutate("Date" = year * 100 + month)
-
-  zoo::as.yearmon
-  df$Date <- as.yearmon(paste(df$year, df$month), "%Y-%m")
-  
-?spread
+  select("basin","report_year","report_month","swsi") %>% #select only columns of interest
+  pivot_wider(names_from = "basin", values_from = "swsi") %>% #pivot to a wider table to match format of past data
+  mutate(Date_Recorded = paste(report_year,report_month,"1", sep = "-")) %>%  #combine year and month columns into one date, add 01 as day
+  select(!(report_year:report_month)) #remove year and month columns
 
 
 View(SWSI2010tonowCLEAN)
-?select
-  
+    
+    

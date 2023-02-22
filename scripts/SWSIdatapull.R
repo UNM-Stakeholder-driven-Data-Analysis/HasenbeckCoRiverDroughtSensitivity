e4c8b6c526b6ecb4ba925c4c592285ff1d8f22be
Eleanor H <eleanorhasenbeck@duck.com>
2023-02-22 09:58:17 -0700

    Adding in SWSI 1981-2011
    Adding data, converting excel file

--- scripts/SWSIdatapull.R
+++ scripts/SWSIdatapull.R
@@ -1,20 +1,30 @@
 ####Read me #### 
-#This script pulls api data from the Colorado DWR SWSI database. 
+#The first part of this script pulls api data from the Colorado DWR SWSI database 
 #Data is for 2010 to present
 #Data source: https://data.colorado.gov/Water/DWR-Surface-Water-Supply-Index-by-Basin/gs59-iraj
 
 
-#### Pulling in data####
+#The second part of this script converts an excel file of historic SWSI data which was shared with me via email on 02/22/2023 by Joel Atwood, a Joel Atwood a Hydrologist with the NRCS's Colorado Snow Survey 
+
+
+#### Pulling in 2010-present data from API####
 library(dplyr)
 library(httr)
 SWSI2010tonow <- GET("https://data.colorado.gov/resource/gs59-iraj.csv")
 SWSI2010tonow = GET("https://data.colorado.gov/resource/gs59-iraj.csv") %>%
   httr::content("parsed")
-View(SWSI2010tonow)
 
-
-####Writing to csv####
-
+###Writing to csv###
 write.csv(SWSI2010tonow,"data/raw/SWSI2010tonow.csv")
 
+#### Converting 1981-2011 SWSI from Excel File to csv #### 
+#install and load readxl package
+install.packages('readxl')
+library(readxl)
 
+#import Excel file into R
+SWSI1981to2011 <- read_excel("data/raw/swsi_data_1981-2011.xlsx")
+
+#Convert jumbled Excel dates to legible dates in R 
+
+View(SWSI1981to2011)

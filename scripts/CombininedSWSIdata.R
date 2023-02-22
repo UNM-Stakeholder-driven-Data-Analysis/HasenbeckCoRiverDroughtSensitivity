04f4c6adba939dcb717ff2021665b2000bd04bd2
Eleanor H <eleanorhasenbeck@duck.com>
2023-02-22 14:31:14 -0700

    Combining SWSI
    New script to clean and combine data

---
+++ scripts/CombininedSWSIdata.R
@@ -0,0 +1,16 @@
+####Read me #### 
+
+#This script first cleans and then combines two SWSI datasets.
+
+#libraries 
+library(tidyverse)
+library(dplyr)
+
+SWSI1981to2011 <- read.csv("data/raw/SWSI1981to2011.csv")
+SWSI2010tonow <- read.csv("data/raw/SWSI2010tonow.csv")
+
+
+####Cleaning SWSI 2010 to now data####
+
+SWSI2010tonowCLEAN <- SWSI2010tonow %>%
+  S

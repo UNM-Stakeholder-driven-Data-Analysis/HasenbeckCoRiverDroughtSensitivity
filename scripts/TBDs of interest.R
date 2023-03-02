7679ddf0d7eba8dfbde8bbbf6438d5f03ff578d0
Eleanor H <eleanorhasenbeck@duck.com>
2023-03-02 11:35:29 -0700

    Tidying TBD data

--- scripts/TBDs of interest.R
+++ scripts/TBDs of interest.R
@@ -15,7 +15,7 @@ library(tidyverse)
 
 
 
-#### Read data ####
+#### Finding relevant WDIDS#### 
 Colorado_Transbasin_Diversions <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/Colorado-Transbasin-Diversions.csv")
 
 #Select only three relevant diversions: 
@@ -25,19 +25,27 @@ TBDs_of_Interest = Colorado_Transbasin_Diversions[Colorado_Transbasin_Diversions
 print(TBDs_of_Interest$Structure_WDID_CSV) # getting relevant water ids
 
 
-
+#### Reading in data from diversions #### 
 #Total diversion = amount of water entering structure
 #Total release = water released from structure under dominion and control 
 
-#CBT: Adams Tunnel: 5104634 (pipe), 0404634 (tunnel) 
-ADAMS_TUNNEL_5104634_Diversions <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/CBT ALVA B ADAMS TUNNEL_5104634_Diversions.csv")
+#CBT: Adams Tunnel: 5104634 (pipe) 5104634 exports transmountain water to 0404634 - ADAMS TUNNEL. Only has diversions. 
+ADAMS_TUNNEL_Diversions <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/CBT ALVA B ADAMS TUNNEL_5104634_Diversions.csv")
+#0404634 (tunnel) This has diversions and releases because it is what the pipe dumps into. NO DATA PRE 1994. Removing from analyis and using only 5104634. 
 ADAMS_TUNNEL_0404634_Diversions_Releases <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/ADAMS TUNNEL_0404634_Diversions_Releases.csv")
 
+
 #Fry-Ark: 3804625 Boustead Tunnel
-BOUSTEAD_3804625_Diversions <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/FRY ARK PR BOUSTEAD TUNNEL_3804625_Diversions.csv")
+BOUSTEAD_Diversions <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/FRY ARK PR BOUSTEAD TUNNEL_3804625_Diversions.csv")
 
 #SJC: 7704636,Blanco diversion, #Navajo 7704635
-NAVAJO_DIVERSION_7704635 <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/USBR NAVAJO DIVERSION_7704635_Diversions_Monthly.csv")
-BLANCO_DIVERSION_2904667 <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/USBR BLANCO R DIVERSION_2904667_Diversions.csv")
+NAVAJO_DIVERSIONS <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/USBR NAVAJO DIVERSION_7704635_Diversions_Monthly.csv")
+BLANCO_DIVERSIONS <- read_csv("~/OneDrive - University of New Mexico/Spring 2023/Stakeholder Analysis/HasenbeckCoRiverDroughtSensitivity/data/raw/USBR BLANCO R DIVERSION_2904667_Diversions.csv")
 
+
+#Simplifying data for analysis 
+ADAMS_TUNNEL_Diversions <- ADAMS_TUNNEL_Diversions %>% select(WDID,`Structure Name`,Date,Amount,`Obs Code`, `Data Status`)
+BOUSTEAD_Diversions <- BOUSTEAD_Diversions%>% select(WDID,`Structure Name`,Date,Amount,`Obs Code`, `Data Status`)
+NAVAJO_DIVERSIONS <- NAVAJO_DIVERSIONS%>% select(WDID,`Structure Name`,Date,Amount,`Obs Code`, `Data Status`)
+BLANCO_DIVERSIONS <- BLANCO_DIVERSIONS%>% select(WDID,`Structure Name`,Date,Amount,`Obs Code`, `Data Status`)
 

11ecb4f2e5c0f2fdc02017251c77c656e2570f34
Eleanor H <eleanorhasenbeck@duck.com>
2023-03-22 16:42:39 -0600

    Cleaning Heron Monthly Data

---
+++ scripts/CleaningHeronData.R
@@ -0,0 +1,37 @@
+
+library(readr)
+library(tidyverse)
+library(lubridate)
+
+HeronReleases <- read_csv(file = "data/raw/Reservoirs/Heron Reservoir and Dam Daily Lake_Reservoir Release - Total-cfs Time Series Data (2008-01-01 - 2023-03-15).csv")
+
+
+HeronReleases <- read.table(file = "data/raw/Reservoirs/Heron Reservoir and Dam Daily Lake_Reservoir Release - Total-cfs Time Series Data (2008-01-01 - 2023-03-15).csv",
+         sep = ";", header=TRUE)
+
+
+CleanHeron <- HeronReleases %>% 
+  select(Result,Datetime..UTC.) %>% #Selecting relevant columns
+  rename(Release = Result, 
+         date_measured = Datetime..UTC.) #renaming columns so they make sense
+
+CleanHeron$Release = as.double(CleanHeron$Release) #Formatting data
+
+CleanHeron$date_measured = parse_date_time(CleanHeron$date_measured, c("%m/%d/%y %H:%M"), exact = T, tz="MST")  #formatting date
+
+
+CleanHeron$yr = lubridate::year(CleanHeron$date_measured)# extract just the year
+CleanHeron$mo = lubridate::month(CleanHeron$date_measured) # extract just the month
+
+HeronMonthly <- CleanHeron %>% #Sum daily data into monthly
+  group_by(yr,mo) %>%
+  summarize(MonthlyRelease = sum(Release))
+
+HeronMonthly$Date = make_date(year = HeronMonthly$yr, month = HeronMonthly$mo, day = 1) #Re-format date column 
+
+HeronMonthlyReleases <- HeronMonthly %>% #Select and rename relevant columns
+  ungroup() %>%
+  select(MonthlyRelease,Date) %>%
+  rename(Release = MonthlyRelease)
+
+write_csv(HeronMonthlyReleases,file = "data/processed/HeronMonthlyReleases") #write to csv

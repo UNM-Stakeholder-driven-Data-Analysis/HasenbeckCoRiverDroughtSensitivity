f13b14c39096b596441a05888abe34861607df37
Eleanor H <eleanorhasenbeck@duck.com>
2023-04-12 17:09:15 -0600

    Squeaky clean script. Also corrected an error in the way SWSI was averaged.

--- scripts/clean scripts/Clean SJC Model Scripts.R
+++ scripts/clean scripts/Clean SJC Model Scripts.R
@@ -1,59 +1,43 @@
 #### read me ####
 
-# This script prepares data for linear modeling and then creates a comparison of a few different model structures. 
+# This script prepares data for linear modeling, creates a few model structures, and plots the best fitting structures based on evaluations in SJC linearmodelautocorrelation script.   
 
 #### libraries ####
 
 library(tidyverse)
 library(lubridate)
 library(forecast)
-library(MARSS)
 library(nlme)
 library(zoo)
-library(beepr)
-library(gridExtra)
 library(lme4)
-library(car)
 library(visreg)
+
+#These libraries didn't get used but may be helpful/are holdovers from past code. 
+#library(car)
+#library(beepr)
+#library(gridExtra)
+#library(MARSS)
 
 
 
 #### load data and format date/time ####
 
+#SWSI#
 SWSI = read.csv("data/processed/SWSI1981to2023.csv", header = T)
 SWSI$Date = as.Date.character(SWSI$Date, format = "%Y-%m-%d")
 
-#### prep time series ####
 
-sum(is.na(SWSI$Date))
-#No NAs. 
+## Azotea ## 
+AzoteaDiversions <- read_csv(file = "data/processed/AzoteaMonthlyDischarge")
+AzoteaDiversions$Date = as.Date(AzoteaDiversions$Date, "%y-%m-%d")
 
-# remove dup time stamps by taking mean of values
-sum(duplicated(SWSI$Date))
-#15 duplicates
+##Heron ## 
+HeronReleases <- read_csv(file = "data/processed/HeronMonthlyReleases") %>%
+  rename("Discharge" = "Release") #using the same column name as in diversion data to simplify replication
 
-SWSI[which(duplicated(SWSI$Date)),]
-SWSI = SWSI %>% 
-  pivot_longer(cols = "Gunnison":"San_Juan", 
-               names_to = "basin",
-               values_to = "SWSI_values") %>%
-  group_by(Date) %>% 
-  summarise(SWSI_values = mean(SWSI_values, na.rm =T))
-
-write_csv(SWSI,file = "data/processed/SWSIduplicatesAvged")
-
-# check for duplicate date/time stamps
-anyDuplicated(SWSI$Date)
-# check percentage of dataset with NAs 
-sum(is.na(SWSI))/nrow(SWSI)*100
-#No NAs! 
-
+HeronReleases$Date = as.Date(HeronReleases$Date, "%y-%m-%d")
 
 #### Azotea - Create time series and remove seasonality ####
-## load data and format date/time ##
-AzoteaDiversions <- read_csv(file = "data/processed/AzoteaMonthlyDischarge")
-
-AzoteaDiversions$Date = as.Date(AzoteaDiversions$Date, "%y-%m-%d")
 
 ## prep time series ##
 sum(is.na(AzoteaDiversions$Date))
@@ -62,7 +46,7 @@ sum(is.na(AzoteaDiversions$Date))
 sum(is.na(AzoteaDiversions$Discharge))
 #46 NAs
 
-# check percentage of dataset with NAs - this is important to document!
+# check percentage of dataset with NAs 
 sum(is.na(AzoteaDiversions))/nrow(AzoteaDiversions)*100
 #7%
 
@@ -147,12 +131,6 @@ ggplot(Discharge_data_DEs, aes(x=Date, y=Discharge))+
 AzoteaDecomp <- Discharge_data_DEs
 
 #### Heron - Create time series and remove seasonality ####
-## load data and format date/time ##
-HeronReleases <- read_csv(file = "data/processed/HeronMonthlyReleases") %>%
-  rename("Discharge" = "Release")
-
-HeronReleases$Date = as.Date(HeronReleases$Date, "%y-%m-%d")
-
 ## prep time series ##
 sum(is.na(HeronReleases$Date))
 #No NAs
@@ -241,24 +219,36 @@ ggplot(Heron_filled, aes(x=Date, y=Discharge))+
 HeronDecomp <- Discharge_data_DEs
 
 #### CO SWSI Prep for modeling ####
-## load data and format date/time ##
-SWSI_CO <- read_csv(file = "data/processed/SWSI1981to2023.csv") %>%
-  dplyr::select(Date,Colorado) %>%
-  rename("SWSI_values" = "Colorado")
+SWSI_CO <- SWSI %>%
+  dplyr::select(Date, Colorado) %>%
+  rename("SWSI_values" = "Colorado") %>% 
+  group_by(Date) %>% 
+  summarize(SWSI_values=mean(SWSI_values)) #Some dates have two entries. Avg the duplicates here. 
 
-SWSI_CO$Date = as.Date(SWSI_CO$Date, "%y-%m-%d")
+# check for duplicate date/time stamps
+anyDuplicated(SWSI_CO$Date)
 
+# check percentage of dataset with NAs 
+sum(is.na(SWSI_CO))/nrow(SWSI_CO)*100
+#No NAs! 
 
 #### RG SWSI - Prep for modeling ####
 ## load data and format date/time ##
-SWSI_RG <- read_csv(file = "data/processed/SWSI1981to2023.csv") %>%
-  dplyr::select(Date,Rio_Grande) %>%
-  rename("SWSI_values" = "Rio_Grande")
+SWSI_RG <- SWSI %>%
+  dplyr::select(Date, Rio_Grande) %>%
+  rename("SWSI_values" = "Rio_Grande") %>% 
+  group_by(Date) %>% 
+  summarize(SWSI_values=mean(SWSI_values)) #Some dates have two entries. Avg the duplicates here. 
 
-SWSI_RG$Date = as.Date(SWSI_RG$Date, "%y-%m-%d")
+# check for duplicate date/time stamps
+anyDuplicated(SWSI_RG$Date)
 
-####Azotea - CO SWSI linear model w seasonal correction on Azotea data   ####
-##Candidates p1q1, p0q2 (<-these two had best BIC comparison), p3, p1q2
+# check percentage of dataset with NAs 
+sum(is.na(SWSI_RG))/nrow(SWSI_RG)*100
+#No NAs! 
+
+####Azotea - CO SWSI linear model w seasonal correction on Azotea data - ARIMA model   ####
+
 Azotea_Decomp_CO_SWSI_Raw <- full_join(AzoteaDecomp,SWSI_CO, by = "Date")  #Combining SWSI by basin with diversion data, Azotea Tunnel, RG SWSI
 Azotea_Decomp_CO_SWSI_Raw$Discharge = as.numeric(Azotea_Decomp_CO_SWSI_Raw$Discharge)
 Azotea_Decomp_CO_SWSI_Raw$SWSI_values = as.numeric(Azotea_Decomp_CO_SWSI_Raw$SWSI_values)
@@ -351,43 +341,42 @@ Acf(resid(mod_AMRAp1q2))
 
 summary(mod_AMRAp1q2)
 
-# #Generalized least squares fit by maximum likelihood
+# Generalized least squares fit by maximum likelihood
 # Model: Discharge ~ SWSI_values 
 # Data: CombinedData 
 # AIC      BIC    logLik
-# 10163.26 10188.67 -5075.632
+# 9877.753 9902.981 -4932.877
 # 
 # Correlation Structure: ARMA(1,2)
 # Formula: ~1 
 # Parameter estimate(s):
 #   Phi1     Theta1     Theta2 
-# 0.5908290 -0.3082652  0.5329362 
+# 0.5879469 -0.3084663  0.5311136 
 # 
 # Coefficients:
-#            Value.   Std.Error    t-value p-value
-# (Intercept) 3526.536  434.4791 8.116700  0.0000
-# SWSI_values  259.312  155.2671 1.670105  0.0955
+#   Value Std.Error  t-value p-value
+# (Intercept) 3551.910  441.1508 8.051464  0.0000
+# SWSI_values  262.156  163.0104 1.608219  0.1084
 # 
 # Correlation: 
 #   (Intr)
-# SWSI_values -0.069
+# SWSI_values -0.045
 # 
 # Standardized residuals:
 #   Min         Q1        Med         Q3        Max 
-# -0.7584343 -0.4978346 -0.2583953  0.1479463 12.5340680 
+# -0.7571319 -0.4974943 -0.2594877  0.1457208 12.4093990 
 # 
-# Residual standard error: 6017.378 
-# Degrees of freedom: 510 total; 508 residual
+# Residual standard error: 6076.267 
+# Degrees of freedom: 495 total; 493 residual
 
 
-####Azotea - RG SWSI linear model w seasonal correction on Azotea data   ####
-#p1q2
+####Azotea - RG SWSI linear model w seasonal correction on Azotea data - ARIMA model   ####
+
 Azotea_Decomp_RG_SWSI_Raw <- full_join(AzoteaDecomp,SWSI_RG, by = "Date")  #Combining SWSI by basin with diversion data, Azotea Tunnel, RG SWSI
 
 #POR for Azotea data is older than for SWSI. Remove dates where there are no SWSI values. 
 Azotea_Decomp_RG_SWSI_Raw <- 
   filter(Azotea_Decomp_RG_SWSI_Raw, Date >= "1981-06-01", Date <= "2022-08-01") 
-Azotea_Decomp_RG_SWSI_Raw <- full_join(AzoteaDecomp,SWSI_CO, by = "Date")  #Combining SWSI by basin with diversion data, Azotea Tunnel, RG SWSI
 Azotea_Decomp_RG_SWSI_Raw$Discharge = as.numeric(Azotea_Decomp_RG_SWSI_Raw$Discharge)
 Azotea_Decomp_RG_SWSI_Raw$SWSI_values = as.numeric(Azotea_Decomp_RG_SWSI_Raw$SWSI_values)
 
@@ -442,8 +431,6 @@ visreg(mod_AMRAp1q2,"SWSI_values")
 Acf(resid(mod_AMRAp1q2))
 
 
-
-
 # extract and assess residuals: AMRAp1q2. Chosing this one because it best satisfies autocorrelation. 
 par(mfrow=c(1,3))
 Acf(resid(mod_AMRAp1q2, type = "normalized"), main="Discharge adjusted, Raw SWSI GLSAMRAp1q2 model residuals")
@@ -455,117 +442,38 @@ Acf(resid(mod_AMRAp1q2))
 AIC(mod_AMRAp1q2)
 summary(mod_AMRAp1q2)
 
-# #Generalized least squares fit by maximum likelihood
+# Generalized least squares fit by maximum likelihood
 # Model: Discharge ~ SWSI_values 
 # Data: CombinedData 
 # AIC      BIC    logLik
-# 10163.26 10188.67 -5075.632
+# 9877.064 9902.291 -4932.532
 # 
 # Correlation Structure: ARMA(1,2)
 # Formula: ~1 
 # Parameter estimate(s):
 #   Phi1     Theta1     Theta2 
-# 0.5908290 -0.3082652  0.5329362 
+# 0.5756965 -0.2958421  0.5413013 
 # 
 # Coefficients:
-#            Value.   Std.Error    t-value p-value
-# (Intercept) 3526.536  434.4791 8.116700  0.0000
-# SWSI_values  259.312  155.2671 1.670105  0.0955
+#   Value Std.Error  t-value p-value
+# (Intercept) 3538.612  436.7897 8.101408  0.0000
+# SWSI_values  278.363  151.4172 1.838384  0.0666
 # 
 # Correlation: 
 #   (Intr)
-# SWSI_values -0.069
+# SWSI_values -0.056
 # 
 # Standardized residuals:
 #   Min         Q1        Med         Q3        Max 
-# -0.7584343 -0.4978346 -0.2583953  0.1479463 12.5340680 
+# -0.7736897 -0.5098446 -0.2519243  0.1300213 12.3585083 
 # 
-# Residual standard error: 6017.378 
-# Degrees of freedom: 510 total; 508 residual
+# Residual standard error: 6048.808 
+# Degrees of freedom: 495 total; 493 residual
 
 
 #first number is autoregressive coef 1
 # middle is differencing data 0
 # last number is moving average term 1
-
-# fit AR(1) regression model with SWSI as a predictor
-mod_Ar1 = gls(Discharge ~ SWSI_values, data=CombinedData, correlation=corAR1(), method="ML")
-
-# fit some other candidate structures
-mod_AMRAp1q1 = gls(Discharge ~ SWSI_values, data=CombinedData, correlation=corARMA(p=1,q=1), method="ML")
-mod_AMRAp2 = gls(Discharge ~ SWSI_values, data=CombinedData, correlation=corARMA(p=2), method="ML")
-mod_AMRAp3 = gls(Discharge ~ SWSI_values, data=CombinedData, correlation=corARMA(p=3), method="ML")
-mod_AMRAp0q2 = gls(Discharge ~ SWSI_values, data=CombinedData, correlation=corARMA(p=0,q=2), method="ML") 
-mod_AMRAp1q2 = gls(Discharge ~ SWSI_values, data=CombinedData, correlation=corARMA(p=1,q=2), method="ML") 
-mod_AMRAp2q2 = gls(Discharge ~ SWSI_values, data=CombinedData, correlation=corARMA(p=2,q=2), method="ML") 
-#p = regressive order, #q is moving average order #41:40#p = regressive order, #q is moving average order #41:40
-
-
-# compare models with BIC
-bbmle::BICtab(mod_Ar1,mod_AMRAp1q1,mod_AMRAp2,mod_AMRAp3,mod_AMRAp0q2,mod_AMRAp1q2,mod_AMRAp2q2)
-bbmle::AICtab(mod_Ar1,mod_AMRAp1q1,mod_AMRAp2,mod_AMRAp3,mod_AMRAp0q2,mod_AMRAp1q2)
-
-# dBIC df
-# mod_AMRAp1q1  0.0 5 
-# mod_AMRAp0q2  0.3 5 
-# mod_AMRAp2    1.2 5 
-# mod_AMRAp3    5.9 6 
-# mod_AMRAp1q2  8.5 6 
-# mod_AMRAp2q2 12.1 7 
-# mod_Ar1      20.1 4 
-
-summary(mod_AMRAp2q2)
-
-
-
-# intervals() for nlme is equivelant to confint() for lm
-intervals(mod_AMRAp2q2)
-
-
-par(mfrow=c(1,1))
-visreg(mod_AMRAp2q2,"SWSI_values")
-
-Acf(resid(mod_AMRAp2q2))
-
-
-# extract and assess residuals: AMRAp1q2. Chosing this one because it best satisfies autocorrelation. 
-par(mfrow=c(1,3))
-Acf(resid(mod_AMRAp1q2, type = "normalized"), main="Discharge adjusted, Raw SWSI GLSAMRAp1q2 model residuals")
-plot(resid(mod_AMRAp1q2, type = "normalized")~c(1:length(CombinedData$SWSI_values)), main="Discharge adjusted, Raw SWSI GLSAMRAp1q2 model residuals"); abline(h=0)
-qqnorm(resid(mod_AMRAp1q2, type = "normalized"), main="Discharge adjusted, Raw SWSI GLSAMRAp1q2 model residuals", pch=16, 
-       xlab=paste("shapiro test: ", round(shapiro.test(resid(mod_AMRAp1q2, type = "normalized"))$statistic,2))); qqline(resid(mod_AMRAp1q2, type = "normalized"))
-
-Acf(resid(mod_AMRAp1q2))
-# 
-# summary(mod_AMRAp1q2)
-# Generalized least squares fit by maximum likelihood
-# Model: Discharge ~ SWSI_values 
-# Data: CombinedData 
-# AIC      BIC    logLik
-# 10163.05 10188.46 -5075.526
-# 
-# Correlation Structure: ARMA(1,2)
-# Formula: ~1 
-# Parameter estimate(s):
-#   Phi1     Theta1     Theta2 
-# 0.5786877 -0.2953304  0.5439725 
-# 
-# Coefficients:
-#                Value Std.Error  t-value p-value
-# (Intercept) 3537.598  430.1511 8.224082  0.0000
-# SWSI_values  260.111  147.9731 1.757827  0.0794
-# 
-# Correlation: 
-#   (Intr)
-# SWSI_values -0.051
-# 
-# Standardized residuals:
-#   Min         Q1        Med         Q3        Max 
-# -0.7679689 -0.5145950 -0.2511092  0.1381228 12.4706044 
-# 
-# Residual standard error: 5995.103 
-# Degrees of freedom: 510 total; 508 residual
-
 
 ####Heron - CO SWSI linear model w seasonal correction on Heron data  ####
 Heron_Decomp_CO_SWSI_Raw <- full_join(HeronDecomp,SWSI_CO, by = "Date")  #Combining SWSI by basin with diversion data, Azotea Tunnel, RG SWSI
@@ -636,21 +544,6 @@ bbmle::BICtab(mod_Ar1,mod_AMRAp1q1,mod_AMRAp2,mod_AMRAp3,mod_AMRAp0q2,mod_AMRAp2
 # mod_Ar1      3.0  4 
 # mod_AMRAp2q2 4.8  7 
 # # mod_AMRAp3   7.0  6 
-
-summary(mod_AMRAp2q2)
-
-
-
-# intervals() for nlme is equivelant to confint() for lm
-intervals(mod_AMRAp2q2)
-
-
-par(mfrow=c(1,1))
-visreg(mod_AMRAp2q2,"SWSI_values")
-
-Acf(resid(mod_AMRAp2q2))
-
-
 
 # extract and assess residuals: Ar1
 par(mfrow=c(1,3))
@@ -753,71 +646,6 @@ bbmle::AICctab(mod_Ar1,mod_AMRAp1q1,mod_AMRAp2,mod_AMRAp3,mod_AMRAp0q2,mod_AMRAp
 bbmle::BICtab(mod_Ar1,mod_AMRAp1q1,mod_AMRAp2,mod_AMRAp3,mod_AMRAp0q2,mod_AMRAp2q2)
 
 
-summary(mod_AMRAp2q2)
-
-
-
-# intervals() for nlme is equivelant to confint() for lm
-intervals(mod_AMRAp2q2)
-
-
-par(mfrow=c(1,1))
-visreg(mod_AMRAp2q2,"SWSI_values")
-
-Acf(resid(mod_AMRAp2q2))
-
-# extract and assess residuals: AMRAp1q1
-par(mfrow=c(1,3))
-Acf(resid(mod_AMRAp1q1, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp1q1model residuals")
-plot(resid(mod_AMRAp1q1, type = "normalized")~c(1:length(CombinedData$SWSI_values)), main=" Discharge adjusted, raw SWSI GLS AMRAp1q1model residuals"); abline(h=0)
-qqnorm(resid(mod_AMRAp1q1, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp1q1model residuals", pch=16, 
-       xlab=paste("shapiro test: ", round(shapiro.test(resid(mod_AMRAp1q1, type = "normalized"))$statistic,2))); qqline(resid(mod_AMRAp1q1, type = "normalized"))
-
-
-
-# extract and assess residuals: AMRAp0q2
-par(mfrow=c(1,3))
-Acf(resid(mod_AMRAp0q2, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp0q2 model residuals")
-plot(resid(mod_AMRAp0q2, type = "normalized")~c(1:length(CombinedData$SWSI_values)), main=" Discharge adjusted, raw SWSI GLS AMRAp0q2 model residuals"); abline(h=0)
-qqnorm(resid(mod_AMRAp0q2, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp0q2 model residuals", pch=16, 
-       xlab=paste("shapiro test: ", round(shapiro.test(resid(mod_AMRAp0q2, type = "normalized"))$statistic,2))); qqline(resid(mod_AMRAp0q2, type = "normalized"))
-
-# extract and assess residuals: #mod_AMRAp2 
-par(mfrow=c(1,3))
-Acf(resid(mod_AMRAp2, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp2 model residuals")
-plot(resid(mod_AMRAp2, type = "normalized")~c(1:length(CombinedData$SWSI_values)), main=" Discharge adjusted, raw SWSI GLS AMRAp2 model residuals"); abline(h=0)
-qqnorm(resid(mod_AMRAp2, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp2 model residuals", pch=16, 
-       xlab=paste("shapiro test: ", round(shapiro.test(resid(mod_AMRAp2, type = "normalized"))$statistic,2))); qqline(resid(mod_AMRAp2, type = "normalized"))
-
-
-
-# extract and assess residuals: mod_AMRAp3 
-par(mfrow=c(1,3))
-Acf(resid(mod_AMRAp3, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp3 model residuals")
-plot(resid(mod_AMRAp3, type = "normalized")~c(1:length(CombinedData$SWSI_values)), main=" Discharge adjusted, raw SWSI GLS AMRAp3 model residuals"); abline(h=0)
-qqnorm(resid(mod_AMRAp3, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp3 model residuals", pch=16, 
-       xlab=paste("shapiro test: ", round(shapiro.test(resid(mod_AMRAp3, type = "normalized"))$statistic,2))); qqline(resid(mod_AMRAp3, type = "normalized"))
-
-
-# extract and assess residuals: AMRAp1q2: 
-par(mfrow=c(1,3))
-Acf(resid(mod_AMRAp1q2, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp1q2 model residuals")
-plot(resid(mod_AMRAp1q2, type = "normalized")~c(1:length(CombinedData$SWSI_values)), main=" Discharge adjusted, raw SWSI GLS AMRAp1q2 model residuals"); abline(h=0)
-qqnorm(resid(mod_AMRAp1q2, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp1q2 model residuals", pch=16, 
-       xlab=paste("shapiro test: ", round(shapiro.test(resid(mod_AMRAp1q2, type = "normalized"))$statistic,2))); qqline(resid(mod_AMRAp1q2, type = "normalized"))
-
-
-
-# extract and assess residuals: AMRAp2q2: 
-par(mfrow=c(1,3))
-Acf(resid(mod_AMRAp2q2, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp2q2 model residuals")
-plot(resid(mod_AMRAp2q2, type = "normalized")~c(1:length(CombinedData$SWSI_values)), main=" Discharge adjusted, raw SWSI GLS AMRAp2q2 model residuals"); abline(h=0)
-qqnorm(resid(mod_AMRAp2q2, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS AMRAp2q2 model residuals", pch=16, 
-       xlab=paste("shapiro test: ", round(shapiro.test(resid(mod_AMRAp2q2, type = "normalized"))$statistic,2))); qqline(resid(mod_AMRAp2q2, type = "normalized"))
-
-
-
-
 # extract and assess residuals: Ar1
 par(mfrow=c(1,3))
 Acf(resid(mod_Ar1, type = "normalized"), main=" Discharge adjusted, raw SWSI GLS Ar1model residuals")
@@ -829,30 +657,30 @@ qqnorm(resid(mod_Ar1, type = "normalized"), main=" Discharge adjusted, raw SWSI 
 
 summary(mod_Ar1)
 
-# Generalized least squares fit by maximum likelihood
+# # Generalized least squares fit by maximum likelihood
 # Model: Discharge ~ SWSI_values 
 # Data: CombinedData 
-# AIC      BIC   logLik
-# 3661.759 3674.641 -1826.88
+# AIC      BIC    logLik
+# 3372.106 3384.649 -1682.053
 # 
 # Correlation Structure: AR(1)
 # Formula: ~1 
 # Parameter estimate(s):
 #   Phi 
-# 0.4500629 
+# 0.3848703 
 # 
 # Coefficients:
 #   Value Std.Error   t-value p-value
-# (Intercept) 4187.806  633.9745  6.605638  0.0000
-# SWSI_values -735.972  257.3048 -2.860311  0.0047
+# (Intercept) 4108.739  604.6264  6.795500  0.0000
+# SWSI_values -759.013  263.1307 -2.884548  0.0044
 # 
 # Correlation: 
 #   (Intr)
-# SWSI_values 0.124 
+# SWSI_values 0.136 
 # 
 # Standardized residuals:
 #   Min         Q1        Med         Q3        Max 
-# -1.3086654 -0.6417618 -0.2551769  0.2928152  6.1761334 
+# -1.3274709 -0.6337984 -0.2557167  0.2739218  6.2644673 
 # 
-# Residual standard error: 5264.006 
-# Degrees of freedom: 185 total; 183 residual
+# Residual standard error: 5193.573 
+# Degrees of freedom: 170 total; 168 residual
